1be125b8b66b497581377ac1cfe321dd960ec260
eugenius <eudriku@gmail.com>
2018-12-02 23:08:09 +0300

    Mock response

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
 from .forms import UploadFileForm
 
 # Create your views here.
@@ -11,7 +10,10 @@ class CharacterExtractor(View):
     def post(self, request):
         form = UploadFileForm(request.POST, request.FILES)
 
+        if form.is_valid():
+            with open('uploads/image.png', 'wb+') as destination:
+                for chunk in request.FILES['file'].chunks():
+                    destination.write(chunk)
 
-
-        result_list = ["Whatever is returned from ML"]
-        render(request, template_name="data_view.html", content_type={"resultlist": result_list})
+            result_list = [{"name": "Kabwama Alvin", "number": 999, "location": "Novia"}]
+            render(request, template_name="data_view.html", content_type={"resultlist": result_list})
