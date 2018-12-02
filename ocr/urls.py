3c61104ab76e3dfc9cdd6541d44275b0964e4a25
eugenius <eudriku@gmail.com>
2018-12-02 22:20:54 +0300

    Add class based view urls

--- ocr/urls.py
+++ ocr/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import CharacterExtractor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('extract/', CharacterExtractor.as_view())
 ]
