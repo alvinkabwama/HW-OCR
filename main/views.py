2fcf83e5002960c374a755e88d86bc8f180f04dc
eugenius <eudriku@gmail.com>
2018-12-02 22:38:54 +0300

    Add file upload form.

--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
+from .forms import UploadFileForm
 
 # Create your views here.
 class CharacterExtractor(View):
     def get(self, request):
-        pass
+        return render(request, template_name="image_upload.html")
+
     def post(self, request):
-        pass
+
+
+        result_list = ["Whatever is returned from ML"]
+        render(request, template_name="data_view.html", content_type={"resultlist": result_list})
