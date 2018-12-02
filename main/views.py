1b5005f2714ad403e737757a64f88f04e8ecf14f
eugenius <eudriku@gmail.com>
2018-12-02 23:50:27 +0300

    Bug fix: context data in upload form template

--- main/views.py
+++ main/views.py
@@ -10,13 +10,12 @@ class CharacterExtractor(View):
 
     def post(self, request):
         form = UploadFileForm(request.POST, request.FILES)
+        # if form.is_valid():
+        with open('uploads/image.png', 'wb+') as destination:
+            for chunk in request.FILES['imagename']:
+                destination.write(chunk)
 
-        if form.is_valid():
-            with open('uploads/image.png', 'wb+') as destination:
-                for chunk in request.FILES['file'].chunks():
-                    destination.write(chunk)
-
-            result_list = [{"name": "Kabwama Alvin", "number": 999, "location": "Novia"}]
-            return render(request, template_name="data_view.html", content_type={"resultlist": result_list})
-        else:
-            return HttpResponse("Not so good!")
+        resultlist = [{"name": "Kabwama Alvin", "number": 999, "location": "Novia"}]
+        return render(request, template_name="data_view.html", context={"resultlist": resultlist})
+        # else:
+        #     return HttpResponse("Not so good!")
