f101d489d20b88cacddfcc0a0cbabb2c8199ca4f
probuse <nprobuse@gmail.com>
2018-12-08 13:26:13 +0300

    rename Urinalysis to UrinalysisInput, add UrinalysisResults

--- urisaf/views.py
+++ urisaf/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import UriSAFForm
 
 # Create your views here.
-class Urinalysis(View):
+class UrinalysisInput(View):
     def get(self, request):
-        return render(request, template_name="data_input.html")
+        return render(request, "data_input.html", {'form': UriSAFForm()})
 
     def post(self, request):
-        print(request.POST.get("nitritesID"))
-        return "hello"
+        form = UriSAFForm(request.POST)
+        if form.is_valid():
+            input_data = dict(
+                leukocytes=form.cleaned_data['leukocytes'],
+                nitrates=form.cleaned_data['nitrites'],
+                uribilinogen=form.cleaned_data['uribilinogen'],
+                protein=form.cleaned_data['protein'],
+                pH=form.cleaned_data['pH'],
+                blood=form.cleaned_data['blood'],
+                specific_gravity=form.cleaned_data['specific_gravity'],
+                ketones=form.cleaned_data['ketones'],
+                bilirubin=form.cleaned_data['bilirubin'],
+                glucose=form.cleaned_data['glucose']
+            )
+        print(input_data)
+        return redirect("urisaf-results")
         
+
+class UrinalysisResults(View):
+    def get(self, request):
+        return render(request, "data_display.html")
